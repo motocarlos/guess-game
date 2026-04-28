3416add841d439d02c725a146c044a63d740817f
motocarlos <127698063+motocarlos@users.noreply.github.com>
2026-04-28 17:01:01 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,29 @@
+const CACHE_NAME = "guess-game-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./firebase.js",
+  "./manifest.json"
+];
 
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => key !== CACHE_NAME && caches.delete(key)))
+    )
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request))
+  );
+});
