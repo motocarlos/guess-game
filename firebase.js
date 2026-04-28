490807db7d44a0d5db2dcf02e851477b38ad8288
motocarlos <127698063+motocarlos@users.noreply.github.com>
2026-04-28 17:04:24 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,10 +1,11 @@
 const firebaseConfig = {
-  apiKey: "DEIN_API_KEY",
-  authDomain: "guess-game.firebaseapp.com",
+  apiKey: "AIzaSyAp36fmoecKjmgmVfewJBdCdoyoRsLm8Pw",
+  authDomain: "guess-game-f2e58.firebaseapp.com",
   projectId: "guess-game-f2e58",
-  storageBucket: "guess-game.appspot.com",
-  messagingSenderId: "DEINE_SENDER_ID",
-  appId: "DEINE_APP_ID"
+  storageBucket: "guess-game-f2e58.firebasestorage.app",
+  messagingSenderId: "656174317689",
+  appId: "1:656174317689:web:84eab9003149a050eed4a8",
+  measurementId: "G-L9GMJGWPS9"
 };
 
 firebase.initializeApp(firebaseConfig);
